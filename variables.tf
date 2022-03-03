529e2226ddbb21781a31cef84c5a43e514e717c0
Apoorva Kulkarni <kuapoorv@amazon.com>
2022-03-03 15:58:47 -0800

    adding addon_context for real

--- variables.tf
+++ variables.tf
@@ -21,3 +21,18 @@ variable "irsa_policies" {
   default     = []
   description = "IAM policy ARNs for Kube State Metrics IRSA"
 }
+
+variable "addon_context" {
+  type = object({
+    aws_caller_identity_account_id = string
+    aws_caller_identity_arn        = string
+    aws_eks_cluster_endpoint       = string
+    aws_partition_id               = string
+    aws_region_name                = string
+    eks_cluster_id                 = string
+    eks_oidc_issuer_url            = string
+    eks_oidc_provider_arn          = string
+    tags                           = map(string)
+  })
+  description = "Input configuration for the addon"
+}
